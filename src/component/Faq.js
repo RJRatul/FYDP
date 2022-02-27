b7ae56f1976ee3b7230735184848581e3abb1f1f
RJRatul <rafsan.ratul.mians@gmail.com>
2022-02-28 05:45:49 +0600

    Faq section edited

--- src/component/Faq.js
+++ src/component/Faq.js
@@ -13,15 +13,15 @@ import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
 import '../component/Faq.css';
 
-function createData(name, calories, fat, carbs, protein) {
-  return { name, calories, fat, carbs, protein };
+function createData(name, address, id, cell, salary) {
+  return { name, address, id, cell, salary };
 }
 const rows = [
-  createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
-  createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
-  createData('Eclair', 262, 16.0, 24, 6.0),
-  createData('Cupcake', 305, 3.7, 67, 4.3),
-  createData('Gingerbread', 356, 16.0, 49, 3.9),
+  createData('Rafiq', 'Dhaka', 1.0, 1833, 4000.0),
+  createData('Faruk', 'Chottagram', 1733, 37, 4000.3),
+  createData('Jobbar', 'Rajshahi', 1933, 24, 6000.0),
+  createData('Alam', 'Khulna', 4.0, 1433, 4000.3),
+  createData('Himu', 'Sylhet', 5.0, 1333, 3000.9),
 ];
 
 const Faq = () => {
@@ -30,112 +30,109 @@ const Faq = () => {
   const handleChange = (panel) => (event, isExpanded) => {
     setExpanded(isExpanded ? panel : false);
   };
-  return(
+  return (
     <div className='faq'>
-      
- 
-  <h1 className='head'>FAQ</h1>
-  <div>
-    <p>
-    <Accordion expanded={expanded === 'panel1'} onChange={handleChange('panel1')}>
-        <AccordionSummary
-          expandIcon={<ExpandMoreIcon />}
-          aria-controls="panel1bh-content"
-          id="panel1bh-header"
-        >
-          <Typography sx={{ width: '33%', flexShrink: 0 }}>
-            General settings
-          </Typography>
-          <Typography sx={{ color: 'text.secondary' }}>I am an accordion</Typography>
-        </AccordionSummary>
-        <AccordionDetails>
-          <Typography>
-            Nulla facilisi. Phasellus sollicitudin nulla et quam mattis feugiat.
-            Aliquam eget maximus est, id dignissim quam.
-          </Typography>
-        </AccordionDetails>
-      </Accordion>
-    </p>
-    <p>
-    <Accordion expanded={expanded === 'panel2'} onChange={handleChange('panel2')}>
-        <AccordionSummary
-          expandIcon={<ExpandMoreIcon />}
-          aria-controls="panel1bh-content"
-          id="panel1bh-header"
-        >
-          <Typography sx={{ width: '33%', flexShrink: 0 }}>
-            General settings
-          </Typography>
-          <Typography sx={{ color: 'text.secondary' }}>I am an accordion</Typography>
-        </AccordionSummary>
-        <AccordionDetails>
-          <Typography>
-            Nulla facilisi. Phasellus sollicitudin nulla et quam mattis feugiat.
-            Aliquam eget maximus est, id dignissim quam.
-          </Typography>
-        </AccordionDetails>
-      </Accordion>
-    </p>
-    <p>
-    <Accordion expanded={expanded === 'panel3'} onChange={handleChange('panel3')}>
-        <AccordionSummary
-          expandIcon={<ExpandMoreIcon />}
-          aria-controls="panel1bh-content"
-          id="panel1bh-header"
-        >
-          <Typography sx={{ width: '33%', flexShrink: 0 }}>
-            General settings
-          </Typography>
-          <Typography sx={{ color: 'text.secondary' }}>I am an accordion</Typography>
-        </AccordionSummary>
-        <AccordionDetails>
-          <Typography>
-            Nulla facilisi. Phasellus sollicitudin nulla et quam mattis feugiat.
-            Aliquam eget maximus est, id dignissim quam.
-          </Typography>
-        </AccordionDetails>
-      </Accordion>
-    </p>
-    
-  
-  </div>
-  <div>
-    <h1 className='head'>Labour List</h1>
-  <TableContainer component={Paper}>
-      <Table sx={{ minWidth: 650 }} aria-label="simple table">
-        <TableHead>
-          <TableRow>
-            <TableCell>Dessert (100g serving)</TableCell>
-            <TableCell align="right">Calories</TableCell>
-            <TableCell align="right">Fat&nbsp;(g)</TableCell>
-            <TableCell align="right">Carbs&nbsp;(g)</TableCell>
-            <TableCell align="right">Protein&nbsp;(g)</TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {rows.map((row) => (
-            <TableRow
-              key={row.name}
-              sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+
+
+      <h1 className='head'>FAQ</h1>
+      <div>
+        <p>
+          <Accordion expanded={expanded === 'panel1'} onChange={handleChange('panel1')}>
+            <AccordionSummary
+              expandIcon={<ExpandMoreIcon />}
+              aria-controls="panel1bh-content"
+              id="panel1bh-header"
             >
-              <TableCell component="th" scope="row">
-                {row.name}
-              </TableCell>
-              <TableCell align="right">{row.calories}</TableCell>
-              <TableCell align="right">{row.fat}</TableCell>
-              <TableCell align="right">{row.carbs}</TableCell>
-              <TableCell align="right">{row.protein}</TableCell>
-            </TableRow>
-          ))}
-        </TableBody>
-      </Table>
-    </TableContainer>
+              <Typography sx={{ width: '20%', flexShrink: 3 }} className='typo'>
+                What is farming?
+              </Typography>
+              <Typography sx={{ color: 'text.secondary' }} className='typo'>Farming is the act or process of working the ground, planting seeds, and growing edible plants.</Typography>
+            </AccordionSummary>
+            <AccordionDetails>
+              <Typography className='typo'>
+                According to the Dictionary definition, “Farming is the act or process of working the ground, planting seeds, and growing edible plants. You can also describe raising animals for milk or meat as farming… Farming is a great way to describe the lifestyle and work of people whose jobs are in the agriculture industry.” The noun “farm,” which derives from the Medieval Latin Firma, originally meant, “A lease on farm land.”
+              </Typography>
+            </AccordionDetails>
+          </Accordion>
+        </p>
+        <p>
+          <Accordion expanded={expanded === 'panel2'} onChange={handleChange('panel2')}>
+            <AccordionSummary
+              expandIcon={<ExpandMoreIcon />}
+              aria-controls="panel1bh-content"
+              id="panel1bh-header"
+            >
+              <Typography sx={{ width: '20%', flexShrink: 3 }} className='typo'>
+                Why is agriculture important?
+              </Typography>
+              <Typography sx={{ color: 'text.secondary' }} className='typo'>griculture is a leading economic source</Typography>
+            </AccordionSummary>
+            <AccordionDetails>
+              <Typography className='typo'>
+                Agriculture plays a crucial role in the economy of developing countries, and provides the main source of food, income, and employment to rural populations. For many countries, agriculture is a leading economic source, with those countries helping to provide some of the US’s food as well.
+              </Typography>
+            </AccordionDetails>
+          </Accordion>
+        </p>
+        <p>
+          <Accordion expanded={expanded === 'panel3'} onChange={handleChange('panel3')}>
+            <AccordionSummary
+              expandIcon={<ExpandMoreIcon />}
+              aria-controls="panel1bh-content"
+              id="panel1bh-header"
+            >
+              <Typography sx={{ width: '20%', flexShrink: 0 }} className='typo'>
+                What is soil health?
+              </Typography>
+              <Typography sx={{ color: 'text.secondary' }} className='typo'>The term “soil health” is used to remind farmers that the soil contains living organisms that help us produce food. </Typography>
+            </AccordionSummary>
+            <AccordionDetails>
+              <Typography className='typo'>
+                Sometimes referred to as soil quality, soil health is the continued ability of soil to act as an ecosystem for plants, animals, and humans. The term “soil health” is used to remind farmers that the soil contains living organisms that help us produce food.
+              </Typography>
+            </AccordionDetails>
+          </Accordion>
+        </p>
 
-  </div>
 
-  
       </div>
-  
+      <div>
+        <h1 className='head'>Labour List</h1>
+        <TableContainer component={Paper}>
+          <Table sx={{ minWidth: 650 }} aria-label="simple table">
+            <TableHead>
+              <TableRow>
+                <TableCell>Name</TableCell>
+                <TableCell align="right">Address</TableCell>
+                <TableCell align="right">ID</TableCell>
+                <TableCell align="right">Cell(+880)</TableCell>
+                <TableCell align="right">Salary(BDT)</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {rows.map((row) => (
+                <TableRow
+                  key={row.name}
+                  sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+                >
+                  <TableCell component="th" scope="row">
+                    {row.name}
+                  </TableCell>
+                  <TableCell align="right">{row.address}</TableCell>
+                  <TableCell align="right">{row.id}</TableCell>
+                  <TableCell align="right">{row.cell}</TableCell>
+                  <TableCell align="right">{row.salary}</TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </TableContainer>
+
+      </div>
+
+
+    </div>
+
   )
 };
 export default Faq
